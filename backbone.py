a6cc660b7f23acedbd6a6cc44e2d2699524ea1f7
Adrian Pardini <publico@tangopardo.com.ar>
2014-01-30 21:01:16 -0300

    adds backbone.js like wrapper over mongo and redis
    Nothing special, just reinventing the square wheel. Again.
    This lets us define models and collections just like in backbone.js that
    interact with our magical redis backend.

---
+++ backbone.py
@@ -0,0 +1,322 @@
+# -*- coding: utf-8 -*-
+
+import re
+import uuid
+import redis, json
+from copy import deepcopy
+
+import pymongo
+from pymongo import MongoClient
+
+from common import *
+
+redis = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+
+_client_id = unicode( uuid.uuid4() )
+
+# XXX: according to mongo docs we only need to create one client and share it everywhere
+client = MongoClient(mongocnstr)
+db = client[dbname]
+
+def deep_get(d, key):
+    """Given a nested dictionary structure and a path like 'a.b.c' tries
+to retrieve it. Returns a tuple of (value, found). If the path is not on
+the structure the returned value is also None"""
+    found = True
+    current = d
+    for k in key.split('.'):
+        if k in current:
+            # current can also be something like "aaaaakbbbb"
+            # so while the 'in' test succeeds you can not access it
+            # like a dict.
+            try:
+                current = current[k]
+            except:
+                found = False
+                break
+        else:
+            found = False
+            break
+
+    if not found:
+        return (None, False)
+    else:
+        return (current, True)
+
+class BackboneException(Exception):
+    pass
+
+class Base(object):
+    """Common stuff for Backbone compatibility.
+It is quite tied for now to our redis+mongo structure"""
+    # some of this will go away once I get sane 'read' support over Redis.
+    colname     = ''
+    backend     = ''
+
+    def __init__(self):
+        if self.colname:
+            self._col = db[self.colname]
+        else:
+            self._col = None
+
+        self._channel = '_RedisSync.' + re.sub('backend$', '', self.backend)
+
+    def sync(self, method, model=None, options=None):
+        # XXX: dumb and not generic. for now it only broadcast to Redis.
+        if method not in 'create read update delete'.split():
+            raise BackboneException('sync(): unknown method %s' % method)
+
+        if model is not None:
+            #XXX: need to consider when we pass a collection or an array of dicts.
+            if isinstance(model, Model):
+                model = model.attributes
+            else:
+                pass
+        else:
+            model = self.attributes
+
+        req = {
+            'method': method,
+            'model': model,
+            '_redis_source': _client_id,
+        }
+        redis.publish(self._channel, json.dumps(req))
+
+class Model(Base):
+    """A class that somehow mimics Backbone.Model
+
+Besides the standard Backbone methods (get, set, update) you can also access it
+like a normal python dict, however it will not trigger change notifications.
+
+To define your models do someting like:
+
+class MyModel(Model):
+    colname = 'the mongo collection' #
+    backend = 'backend name' #used for live sync over Redis.
+    defaults = {} # optional dictionary with default values.
+"""
+    idAttribute = '_id'
+    id          = None
+    defaults    = {}
+
+    def __init__(self, attributes=None):
+        super(Model, self).__init__()
+        self.collection = None
+        self.attributes = deepcopy(self.defaults)
+
+        if attributes is not None:
+            self.attributes.update(attributes)
+            self.id = attributes.get(self.idAttribute, None)
+
+    def __getitem__(self, key):
+        return self.attributes[key]
+
+    def __setitem__(self, key, value):
+        self.attributes[key] = value
+
+    def __delitem__(self, key):
+        del( self.attributes[key] )
+
+    def __iter__(self, *args, **kwargs):
+        return iter(self.attributes)
+
+    def __contains__(self, item):
+        return item in self.attributes
+
+    def keys(self):
+        return self.attributes.keys()
+
+    def values(self):
+        return self.attributes.values()
+
+    def items(self):
+        return self.attributes.items()
+
+    def iterkeys(self):
+        return self.attributes.iterkeys()
+
+    def itervalues(self):
+        return self.attributes.itervalues()
+
+    def iteritems(self):
+        return self.attributes.iteritems()
+
+    def get(self, *args, **kwargs):
+        return self.attributes.get(*args, **kwargs)
+
+    def pop(self, *args, **kwargs):
+        return self.attributes.pop(*args, **kwargs)
+
+    def popitem(self, *args, **kwargs):
+        return self.attributes.popitem(*args, **kwargs)
+
+    def setdefault(self, *args, **kwargs):
+        return self.attributes.setdefault(*args, **kwargs)
+
+    def update(self, *args, **kwargs):
+        return self.attributes.update(*args, **kwargs)
+
+    def set(self, attributes, options=None):
+        self.attributes = attributes.copy()
+        self.id = self.attributes.get(self.idAttribute, None)
+
+    def fetch(self, options=None):
+#XXX: need to move all of this into a generic sync.
+        if self._col is None:
+            return
+
+        # need to keep old attributes and fire a change or something.
+        _id = self.id
+        if _id is not None:
+            ret = self._col.find_one({'_id': _id})
+        else:
+            ret = self._col.find_one()
+
+        self.attributes = deepcopy(self.defaults)
+        if ret is not None:
+            self.attributes.update(ret)
+
+    def save(self, attributes=None, options=None):
+        if attributes is not None:
+            self.attributes.update(attributes)
+
+        method = 'update'
+        if self.id is None:
+            # ObjectID gives a lot of trouble when(if) we want to send it over Redis.
+            self.id = self.attributes.get(self.idAttribute, None) or unicode( uuid.uuid4() )
+            self.attributes[self.idAttribute] = self.id
+            method = 'create'
+
+        if self._col:
+            self._col.update({'_id': self.id}, self.attributes, True)
+        self.sync(method)
+
+    def destroy(self, options=None):
+        if self.id is not None:
+            if self._col:
+                self._col.remove({'_id': self.id})
+            self.sync('delete')
+
+        if self.collection:
+            self.collection.remove(self)
+
+
+class Collection(Base):
+    """A class that somehow mimics Backbone.Collection
+Besides the standard Backbone methods (get, set, update, find, findWhere)
+you can also access it like a normal python dict, however it will not trigger
+change notifications.
+
+To define your collections do someting like:
+
+class MyCollection(Collection):
+    colname = 'the mongo collection' #
+    backend = 'backend name' # used for live sync over Redis.
+    model   = MyModel
+"""
+
+    model = Model
+
+    def __init__(self, models=None, options=None):
+        super(Collection, self).__init__()
+
+        self.models = []
+        self._models = {}
+
+        if models is not None:
+            for m in models:
+                if isinstance(m, Model):
+                    M = m
+                else:
+                    M = self.model(m)
+
+                self.models.append(M)
+                self._models[M.id] = M
+                M.collection = self
+
+    def __iter__(self, *args, **kwargs):
+        return iter( self.models )
+
+    def __contains__(self, item):
+        return item in self.models
+
+    def __len__(self):
+        return len(self.models)
+
+    def __getitem__(self, key):
+        return self.models[key]
+
+    def get(self, _id):
+        return self._models[_id]
+
+    def where(self, attributes, _all=True):
+#XXX: probably it would be better to retunr a generator here but then there is
+# the case when we modify the stuff we are iterating over.
+        def cmp(m):
+            for k,v in attributes.iteritems():
+                if k not in m:
+                    return False
+                if m[k] != v:
+                    return False
+            return True
+
+        if _all:
+            return [ m for m in self if cmp(m) ]
+        else:
+            for m in self:
+                if cmp(m):
+                    return m
+
+    def findWhere(self, attributes):
+        return self.where(attributes, False)
+
+#XXX:
+    def fetch(self, options=None):
+        # XXX: need to keep old attributes and fire a change or something.
+        # XXX: need to implement the merging behaviour inside set(). For now we just reset.
+
+        self.models = []
+        self._models = {}
+
+        for m in self._col.find():
+            M = self.model(m)
+            self.models.append(M)
+            self._models[M.id] = M
+            M.collection = self
+
+
+    def add(self, m, options=None):
+        if isinstance(m, Model):
+            M = m
+        else:
+            M = self.model(m)
+
+        if M in self.models:
+            return
+
+        self.models.append(M)
+        self._models[M.id] = M
+        M.collection = self
+
+    def remove(self, model, options=None):
+        def _remove(m):
+            if m not in self:
+                return
+            self.models.remove(m)
+            del( self._models[m.id] )
+
+        if isinstance(model, Model):
+            _remove(model)
+        else:
+            for m in models:
+                _remove(m)
+
+#XXX:
+    def create(self, attributes, options=None):
+        if not isinstance(attributes, Model):
+            model = self.model(attributes)
+        else:
+            model = attributes
+
+        model.save()
+        self.add(model)
